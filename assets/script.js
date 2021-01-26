c0cefb430e64c26fb55086d28f67143b1a4aecb3
Ethan Dutcher <74162204+edutcher@users.noreply.github.com>
2021-01-26 01:53:04 -0500

    added functions to move map and populate popups. added API call for initial location

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,34 @@
-var map = L.map('map').setView([51.505, -0.09], 18);
+var map;
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+$.ajax({
+    url: `https://json.geoiplookup.io/`,
+    type: "GET"
+}).then(function(res) {
+    const { latitude, longitude } = res;
+    map = L.map('map').setView([latitude, longitude], 18);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
+    //getBrews(res.city, res.region);
+}).catch((er) => {
+    console.log(er);
+})
+
+function moveMap(lat, lon) {
+    map.panTo(new L.LatLng(lat, lon));
+}
+
+function popMap(places) {
+    for (var i = 0; i < places.length; i++) {
+        let lat = places[i].latitude;
+        let lon = places[i].longitude;
+        L.marker([lat, lon]).addTo(map)
+            .bindPopup(`${places[i].name}`)
+            .openPopup();
+    }
+}
 
 $(document).ready(function() {
     var elems = document.querySelectorAll('.carousel');
