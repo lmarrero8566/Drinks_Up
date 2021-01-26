2a9db9fad8a9e813c6f0552dfe5a23a5e62ff03d
Ethan Dutcher <74162204+edutcher@users.noreply.github.com>
2021-01-25 22:03:43 -0500

    HTML Skeleton

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+var myVar = "hello";
+
+$(document).ready(function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems);
+    var el = document.querySelectorAll('.tabs');
+    var instance = M.Tabs.init(el);
+});
