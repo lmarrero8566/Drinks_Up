171d440323c3fc95f3b4f05e9d1db004b91eae29
FarisKadir <faris.kadir@gmail.com>
2021-02-01 20:59:33 -0500

    Finished forming the queryURL when multiple options are selected. Finished storing an arry that contains all of the search resutls.

--- brewery.js
+++ brewery.js
@@ -1,82 +1,127 @@
-//To Do
-    // Dynamically build queryURl based on what inputs were provided. - Done
-    // Parse response array into separate objects for each brewery. - Done
-        //The object should contain:
-        //     Name - Should link to the website Url
-        //     city
-        //     state
-        //     zip
-        //     phone
-        //     type
-        //     Location button - will trigger the pinpoint on the map
-
-//Display the results on the html page - Not Started
-
-//Nice to Have
-    //If no latitude or longitude is available use the street address to retrieve the latitude and longitude.
-
-
 $(document).ready(function()    {
-    var bdbQueryURL = "https://api.openbrewerydb.org/breweries";
 
-    var resObj = [];
+    //Declaring all global variables
+    var bdbQueryURL = ["https://api.openbrewerydb.org/breweries"];
 
-    //API Request to open brewery
+    //Empty array that will be used to store all of the results from the API GET request
+    var resultsObj = [];
 
-    // This should be a jquery selector using the ID for the Name input field.
-    var brewName = $("#name").val(); 
+    //Variables for all the inputs
+    var brewName = {
+        param: "?by_name=",
+        value: $("#brewname").val()
+    };
 
-    // This should be a jquery selector using the ID for the Zip code input field.
-    var zip = "?by_zip=34787"; // Used this value for testing. Will pull this from the input field from the front-end.
+    var brewType = {
+            param: "?by_type=",
+            value: $("#brewtype").val()
+    };
 
-    // This should be a jquery selector using the ID for the city input field.
-    var city = "?by_city=Orlando";   // Used this value for testing. Will pull this from the input field from the front-end.
+    var brewCity = {
+        param: "?by_city=",
+        value: "" //$("#cityname").val() 
+    };     
 
-    // This should be a jquery selector using the ID for the state input field.
-    var state =  "?by_state=Florida";  // Used this value for testing. Will pull this from the input field from the front-end.
+    var brewState =  {
+        param: "?by_state=",
+        value: "Florida" //$("#state").val()
+    };  
 
-    // This should be a jquery selector using the ID for the type input field.
-    var type = $("#type").val(); 
+    var brewZip = {
+        param: "?by_postal=",
+        value: "" //$("#zip").val()
+    };
 
-    var criteria = [brewName, zip, city, state, type];
+    //Query URL array. This allows us to take in multiple optional criteria
+    var brewInput = [];
 
-    console.log(criteria);
+    //Query that checks for null values. It will only include values that are not null into the query url array
+    function nullCheck(varName)  {
+        if (varName.value !== "")   {
+            brewInput.push(varName);
+        };
+        return brewInput;
+    };
 
-    //Forms the query url and includes any search criteria that was inputted by the user.
-    function formBdbQueryURL()  {
-        for (var i = 0; i < criteria.length; i++)   {
-            if (criteria[i])  {
-                    bdbQueryURL = bdbQueryURL + criteria[i];
-            }
+    function formQueryURL() {
+        //This checks to see if each variable has a value. if it does, then it's added to the array.
+        nullCheck(brewName);
+        nullCheck(brewType);
+        nullCheck(brewCity);
+        nullCheck(brewState);
+        nullCheck(brewZip);
+        console.log(brewInput);
+        if (brewInput.length === 0) {
+            $("brewresults").append($("<p>"));
+            console.log("you must provide at least one criteria");
         }
-        return;
-    }
+        else    {
+            //adds the first param to the query string so that all of the remaining params can include the & symbol
+            bdbQueryURL = bdbQueryURL + brewInput[0].param + brewInput[0].value;
+            brewInput.splice(0,1);
+            //this iterates through the remaining objects in the array and adds them to the queryURL.
+            brewInput.forEach(function(thisEl)  {
+                bdbQueryURL = bdbQueryURL + "&" + thisEl.param + thisEl.value;
+            });
+            return bdbQueryURL;
+        };
+    };
 
+    //Makes the API Call
+    function openBrew() {
 
-    formBdbQueryURL();
+        // //API request
+        $.ajax({    
+            url: bdbQueryURL,
+            method: "GET"
+            }) .then (function(response) {
+                    popMap(response);
+                    $(response).each(function(thisEl)   {
+                        obj = {
+                            name: response[thisEl].name,
+                            street_1: response[thisEl].street,
+                            street_2: response[thisEl].address_2,
+                            street_3: response[thisEl].address_3,
+                            city: response[thisEl].city,
+                            state: response[thisEl].state,
+                            zip: response[thisEl].postal_code,
+                            phone: response[thisEl].phone,
+                            lon: response[thisEl].longitude,
+                            lat: response[thisEl].latitude,
+                            website: response[thisEl].website_url,
+                        };
+                        resultsObj.push(obj);
+                        return resultsObj;
+                    })
+                });
+                return resultsObj;
+    };
 
-
-    // //API request
-    $.ajax({    
-    url: bdbQueryURL,
-    method: "GET"
-    }) .then (function(response) {
-            popMap(response);
-            $(response).each(function()   {
-                obj = {
-                    name: this.name,
-                    city: this.city,
-                    state: this.state,
-                    zip: this.postal_code,
-                    phone: this.phone,
-                    lon: this.longitude,
-                    lat: this.latitude,
-                    website: this.website_url
-                };
-                resObj.push(obj);
-            })
+    //Creates buttons for each of the results
+    function renderResults(arr)    {
+        arr.forEach(function(thisEl) {
+            var div = $("<div>");
+            var span = $("<span>");
+            var btn = $("<button>");
+            div.addClass("card horizontal");
+            span.addClass("card-stacked");
+            btn.addClass("btn-small");
+            $("#brewresults").append(div);
+            div.append(span);
+            span.append(btn);
+            btn.text(arr[thisEl].name + "\nAddress:\n" + thisEl.street_1 + "\n " + thisEl.street_2 + "\n " + thisEl.city + "," + thisEl.state + "," + thisEl.zip);
+            console.log("this worked");
         });
+    };
 
-        console.log(resObj);
 
-})
+    //This will occur with the onclick event
+    // formQueryURL();
+    // console.log(bdbQueryURL);
+    // openBrew();
+    // console.log(resultsObj);
+    // renderResults(resultsObj);
+
+
+
+});
