8f290df360abd8565ec180e9d4fbaa71f63dd6e8
Ethan Dutcher <74162204+edutcher@users.noreply.github.com>
2021-02-01 19:53:35 -0500

    hide map/carousel

--- assets/index.js
+++ assets/index.js
@@ -5,6 +5,14 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.FormSelect.init(select);
     var tabs = document.querySelectorAll('.tabs');
     var instance = M.Tabs.init(tabs);
-  });
 
-  
+    $('#cocktailTab').on('click', function() {
+        $('.showcase').removeClass('hidden');
+        $('#mapHide').addClass('hidden');
+    })
+
+    $('#breweryTab').on('click', function() {
+        $('.showcase').addClass('hidden');
+        $('#mapHide').removeClass('hidden');
+    })
+});
